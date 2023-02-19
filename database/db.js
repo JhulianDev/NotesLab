aeef396637e4e6edb420e11856833dfb54660b89
JhulianCode <aguilera.jhulian@gmail.com>
2023-02-19 06:11:27 -0300

    Correction of db.js

--- database/db.js
+++ database/db.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 })
 
 // Establecemos la conexión con la base de datos
